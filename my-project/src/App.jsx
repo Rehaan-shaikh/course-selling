4231a68bf1d8455a671e7e77181da2123b7a2725
Maaz-Shaikh-413 <shaikhmaaz8788@gmail.com>
2025-01-16 15:16:37 +0530

    project initalization completed

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react"
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
